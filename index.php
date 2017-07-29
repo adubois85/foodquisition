d7912ef706aec4054e40e775e72650602054fb17
Alexander DuBois <adubois@alumni.uci.edu>
2017-07-29 12:54:42 -0600

    Created basic index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>¡Ayúdame! ABQ</title>
+	</head>
+	<body>
+		<p>This will be the main body of the file; feel free to delete this.</p>
+	</body>
+</html>
