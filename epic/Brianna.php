331ed56dd2a2ffec4fc50feed7cc4645cfcaa2b4
dbranch6 <dbranch6@cnm.edu>
2017-07-29 15:37:13 -0600

    added frustrations and needs and also goal section

--- epic/Brianna.php
+++ epic/Brianna.php
@@ -11,6 +11,7 @@
 		<p>Technology: Brianna uses an LG Harmony with Android 7 Nougat with a 5.3" HD screen for almost everything, occasionally she will use the desktops at school or the library.</p>
 		<p>Attitudes and behaviour: Brianna is busy most of the time and stays higly stressed.
 		She spends her lunch breaks and between class time to look for resources so that she can afford to stay in school. Brianna is looking to be able to contact agencies during this down time as well. </p>
-		<p>Frustrations and needs: </p>
+		<p>Frustrations and needs: Brianna has limited time to search for and then contact agencies that provide the services that she needs. Brianna is looking for somewhere that she can not only find the resources she needs but also be able to contact said agency when she does find them. If Brianna is not able to find help to pay her bills or provide food to her household she will have to drop out of school to provide for these needs.</p>
+		<p>Goals: Brianna is wanting to find the services she needs and connect to them quickly and simply.</p>
 	</body>
 </html>
