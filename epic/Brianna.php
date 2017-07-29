261565fe6f0aad83c3c7eb9560273d5735c69112
dbranch6 <dbranch6@cnm.edu>
2017-07-29 15:29:42 -0600

    Starting the first persona rough draft

---
+++ epic/Brianna.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Brianna Smith Persona</title>
+		<body>
+		<h1>Persona</h1>
+		<p>Name: Brianna Smith</p>
+		<p>Age: 29</p>
+		<p>Profession: Stay at home mother and full-time student studying nursing. Brianna has four children and her only income is financial aid so all of her free time is dedicated to studying and child rearing.</p>
+		<p>Technology: Brianna uses an LG Harmony with Android 7 Nougat with a 5.3" HD screen for almost everything, occasionally she will use the desktops at school or the library.</p>
+		<p>Attitudes and behaviour: Brianna is busy most of the time and stays higly stressed.
+		She spends her lunch breaks and between class time to look for resources so that she can afford to stay in school. Brianna is looking to be able to contact agencies during this down time as well. </p>
+		<p>Frustrations and needs: </p>
+	</body>
+</html>
