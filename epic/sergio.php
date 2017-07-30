89aafe8deb5836465cd5b0aca61c74e3279d3eca
Steve Stone <stoneflynm@gmail.com>
2017-07-30 15:35:27 -0600

    added sergio persona

---
+++ epic/sergio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Brianna Smith Persona</title>
+	<body>
+		<h1>Persona</h1>
+		<p>Name: Sergio Garcia</p>
+		<p>Age: 38</p>
+		<p>Profession: For most of his adult life Sergio has worked in kitchens. He attended culinary school in Austin, TX
+			when he was 25. More recently he has been diagnosed with diabetes which makes it hard for him to stand all day
+			like he used to when he was younger; because of this, he has a hard time holding down a job. There are frequent
+			days when he can't walk due to the pain in his legs and he has to call in sick to work; which in turn, frequently
+			leaves him without gainful employment.
+		</p>
+		<p>Technology:</p>
+		<p>Attitudes and behaviour: </p>
+		<p>Frustrations and needs:</p>
+		<p>Goals:</p>
+	</body>
+</html>
