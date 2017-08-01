1e46b9ab0e9b52e1cff31f49d7b5e3fa1c47f8d8
dbranch6 <dbranch6@cnm.edu>
2017-08-01 15:29:16 -0600

    Changed name

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<title>¡Ayúdame! ABQ</title>
+		<title>Foodquisition</title>
 	</head>
 	<body>
 		<p>Main Section just testing my deployment</p>
