63457a9fe93ef755738dcf5020f28a1f18714a9e
dbranch6 <dbranch6@cnm.edu>
2017-08-01 16:05:42 -0600

    Changed user-stories1.php to brianna-usecase.php

--- epic/brianna-usecase.php
+++ epic/brianna-usecase.php
@@ -9,5 +9,15 @@
 		<p>Brianna has been thinking of taking her family to a restaurant called Food Karma here in Albuquerque. She loves the social aspect of the restaurant and the reviews she has read have been mostly positive. </p>
 		<p>She has looked at Yelp and Google reviews but would really like more substantial data with more meat on the bone. The review sites she has looked at are mostly personal opinions and she is looking for more fact based data.</p>
 		<p>It is 4pm and her family is getting ready to go. She is short on time and wants to be able to quickly decide if Food Karma is up to her standards. She has one hour to find the information she needs and make a decision on whether to go to Food Karma or pick somewhere else to dine.</p>
+		<h2>Interaction Flow</h2>
+		<ul>
+			<li> Brianna enters the Url of foodQuisition </li>
+			<li> Brianna is brought to the home screen search page with various search fields.</li>
+			<li> Brianna enters restaurant name or address and presses enter.</li>
+			<li> The website will then query the database. </li>
+			<li> The database will then return information based on information Brianna entered.</li>
+			<li>The website will then display a card per matching database entry.</li>
+			<li>Brianna will then be able to pick a card and it would expand giving more indepth information.</li>
+		</ul>
 	</body>
 </html>
