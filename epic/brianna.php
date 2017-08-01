aaa9dd0e62e2c1d3d393b3047067e5a804cc58ba
dbranch6 <dbranch6@cnm.edu>
2017-07-31 20:26:40 -0600

    finished new brianna.php

--- epic/brianna.php
+++ epic/brianna.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="utf-8"/>
 		<title>Brianna Smith Persona</title>
+	</head>
 		<body>
 		<h1>Persona</h1>
 		<p>Name: Brianna Smith</p>
 		<p>Age: 29</p>
-		<p>Profession: Stay at home mother and full-time student studying nursing. Brianna has four children and her only income is financial aid so all of her free time is dedicated to studying and child rearing.</p>
+		<p>Profession: Stay at home mother and full-time student studying nursing. Brianna has four children. She is very busy and eats out alot or has food delivered. Her studies make it hard for her to have the time to prepare food.</p>
 		<p>Technology: Brianna uses an LG Harmony with Android 7 Nougat with a 5.3" HD screen for almost everything, occasionally she will use the desktops at school or the library.</p>
-		<p>Attitudes and behaviour: Brianna is busy most of the time and stays higly stressed.
-		She spends her lunch breaks and between class time to look for resources so that she can afford to stay in school. Brianna is looking to be able to contact agencies during this down time as well. </p>
-		<p>Frustrations and needs: Brianna has limited time to search for and then contact agencies that provide the services that she needs. Brianna is looking for somewhere that she can not only find the resources she needs but also be able to contact said agency when she does find them. If Brianna is not able to find help to pay her bills or provide food to her household she will have to drop out of school to provide for these needs.</p>
-		<p>Goals: Brianna is wanting to find the services she needs and connect to them quickly and simply.</p>
+		<p>Attitudes and behaviour: Brianna is busy most of the time and stays highly stressed.
+		She hates having to feed her kids takeout and restaurant food all the time and is looking for a way to make sure these establishments she visits are up to her standards.</p>
+		<p>Frustrations and needs: Brianna has limited time to search for and make sure these places are up to par. Brianna is looking for somewhere that she can not only find the information she needs but also be able to see what the actual issue was if any.</p>
+		<p>Goals: Brianna is wanting to find the information she needs with easy to understand information without having to dig through the cities inspection data.</p>
 	</body>
 </html>
