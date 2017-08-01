a56c7f3c0f618d658528e3d04f630e4ebf2372fa
Steve Stone <stoneflynm@gmail.com>
2017-07-31 21:15:22 -0600

    finished frustrations and needs for sergio

--- epic/sergio.php
+++ epic/sergio.php
@@ -7,23 +7,13 @@
 		<h1>Persona</h1>
 		<p>Name: Sergio Garcia</p>
 		<p>Age: 38</p>
-		<p>Profession: Sergio has been working at Blue Cross Blue Shield for the last seven years. He is a customer support
-			specialist. Everyday he answers customer questions about thier medical bills. The questions are generally simple
-			but the answers are not always as simple. For instance, explaining why someone's insurance did not cover all the
-			costs for a specific blood test is not always easy. Many times people become irrate and it is up to Sergio to
-			calm them down. He is very good at his job and usually enjoys his time at work.</p>
-		<p>Technology: Since Sergio finds himself frequently unemployed, he does not have regular acces to his iPhone 5 but
-			he does own an Dell laptop; it's about five years old. Sergio a regular Facebook user but is usually confined
-			posting without adding any pictures since his iPhone is usually not in service.
+		<p>Profession: Sergio has been working at Blue Cross Blue Shield for the last seven years. He is a customer support specialist. Everyday he answers customer questions about thier medical bills. The questions are generally simple but the answers are not always as simple. For instance, explaining why someone's insurance did not cover all the costs for a specific blood test is not always easy. Many times people become irrate and it is up to Sergio to them down. He is very good at his job and usually enjoys his time at work.
 		</p>
-		<p>Attitudes and Behaviour: When he does have a job, he shares a 2004 Ford F150 with his wife as his main form of
-			transportion. If his wife is using the truck he will usually take the city bus to get to work. Sergio does enjoy
-			working in restaurants but as he gets older he finds his diabetes affects him more and more often. He is worried
-			that, sooner rather than later, he will be forced into medical retirement, something he is not prepared for.
+		<p>Technology: More often than not, Sergio uses his three year old Toshiba Satellite laptop to access the internet and stay connected with his friends. He does own an iPhone 5 but since he does not have unlimited data he is very cautious about using his phone when he is not connected to wifi.
 		</p>
-		<p>Frustrations and Needs: As it is, his wife and him are not always able to pay rent on time and are sometimes
-			left without enough money to eat healthy which only compounds his medical condition. They have received financial
-			support from the state in the past but again they are usually not able to afford the more healthy options.
+		<p>Attitudes and Behaviour: On his days off, Sergio enjoys spending his time trying out new restaurants. Although he has never worked in a restaurant, Sergio has spent plenty of time and money exploring the restaurants in his city. He is willing to try anything, or any place, at least once. And because Sergio doesn't always know what he is eating, he finds comfort in knowing the restaurants he eats at have exemplary health inspections.
+		</p>
+		<p>Frustrations and Needs: Sergio remembers when he was a kid living in California that restaurants would post their inspection score on the windows of their store front. He has always liked that model and wishes there was an easy way for him to check the latest inspection results of the restaurants he eats at, especially the new ones.
 		</p>
 		<p>Goals: Sergio needs to be able to find other sources of aid, whether financial or subsistence. If possible, he
 			would like to be able to quickly search for available options from his laptop before he leaves his apartment.
