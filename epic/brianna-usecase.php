9b4eb097dca10eb974cbcd8b2d995f2940038258
dbranch6 <dbranch6@cnm.edu>
2017-08-01 15:44:45 -0600

    Changed user-stories1.php to brianna-usecase.php

--- epic/brianna-usecase.php
+++ epic/brianna-usecase.php
@@ -9,6 +9,5 @@
 		<p>Brianna has been thinking of taking her family to a restaurant called Food Karma here in Albuquerque. She loves the social aspect of the restaurant and the reviews she has read have been mostly positive. </p>
 		<p>She has looked at Yelp and Google reviews but would really like more substantial data with more meat on the bone. The review sites she has looked at are mostly personal opinions and she is looking for more fact based data.</p>
 		<p>It is 4pm and her family is getting ready to go. She is short on time and wants to be able to quickly decide if Food Karma is up to her standards. She has one hour to find the information she needs and make a decision on whether to go to Food Karma or pick somewhere else to dine.</p>
-
 	</body>
 </html>
