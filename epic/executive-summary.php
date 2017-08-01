3f3d7ebf72236bc07ee06e039f077f84748de514
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-07-31 20:24:18 -0600

    remembering to push correction

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -3,6 +3,6 @@
 	<head>Executive Summary</head>
 		<body>
 			<p>Our vision is to create a website that tells Albuquerque residents of the inspection results at their
-				favorite restaurants allowing them to make better informed dining choices   </p>
+				favorite restaurants allowing them to make better informed dining choices </p>
 	</body>
 </html>
