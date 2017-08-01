59323e992782c3d9b19198d287283a35fa47f2a1
dbranch6 <dbranch6@cnm.edu>
2017-07-31 20:42:10 -0600

    Started brianna use case

---
+++ epic/user-stories1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Brianna Use Case</title>
+	</head>
+	<body>
+		<h1>Use Case</h1>
+		<p>Brianna has been thinking of taking her family to a restaurant called Food Karma here in Albuquerque. She loves the social aspect of the restaurant and the reviews have been mostly positive. </p>
+
+	</body>
+</html>
