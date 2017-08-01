ae51cb4f266b442cd8e9c1a2434afa2929a6cc77
Steve Stone <stoneflynm@gmail.com>
2017-08-01 16:42:39 -0600

    finished sergio interaction flow

--- epic/sergio-usecase.php
+++ epic/sergio-usecase.php
@@ -12,6 +12,17 @@
 		<p>
 			After going back and forth with ideas they have decided on a place to go, Saigon. It is a very popular Vietnamese restaurant. The reviews on Yelp are very good and they are both looking forward to trying a new place. Joe will be at Sergio's apartment in 20 minutes. Before Joe gets there, Sergio would like to search for the latest helath inspection results so he can focus on the food and not worry about how clean or dirty or unsanitary the kitchen is. Sergio goes to foodquisition.com armed with the restaurant name.
 		</p>
+		<h2>Interaction Flow</h2>
+		<ul>
+			<li> Sergiio enters the Url of foodQuisition.</li>
+			<li> Sergio is brought to the home screen search page with various search fields: genre, zip code, name.</li>
+			<li> Sergio enters restaurant name and presses enter.</li>
+			<li> The website runs the query on the database. </li>
+			<li> The database then returns the information based on the name Sergio entered.</li>
+			<li> The website will then display a web page a card matching the database entry.</li>
+			<li> Sergio will then be able to click on the card and it would expand giving more in detailed information.
+			</li>
+		</ul>
 
 	</body>
 </html>
