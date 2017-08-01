09c9b2a656faaf05f85935ad3bd855b48d2217c4
Steve Stone <stoneflynm@gmail.com>
2017-08-01 16:06:19 -0600

    working through sergio use case

--- epic/sergio-usecase.php
+++ epic/sergio-usecase.php
@@ -7,7 +7,10 @@
 	<body>
 		<h1>Use Case</h1>
 		<p>
-
+			After a long day of working at Blue Cross/Blue Shield, Sergio has just finished his first Bosque Brewing Scotia Scotch Ale and his stomach is starting to rumble. He is starting to think his plans for the evening and going out  to eat at a restaurant is first on his list. His phone starts to vibrate. It's Joe, one of his best friends. Joe suggests that they try out a place that they have both never been before.
+		</p>
+		<p>
+			After going back and forth with ideas they have decided on a place to go
 		</p>
 
 	</body>
