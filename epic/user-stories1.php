134cac6cecc7c508afbf2e7c78b49f714cbfee6e
dbranch6 <dbranch6@cnm.edu>
2017-07-31 20:48:26 -0600

    added second paragraph

--- epic/user-stories1.php
+++ epic/user-stories1.php
@@ -7,6 +7,7 @@
 	<body>
 		<h1>Use Case</h1>
 		<p>Brianna has been thinking of taking her family to a restaurant called Food Karma here in Albuquerque. She loves the social aspect of the restaurant and the reviews have been mostly positive. </p>
+		<p>She has looked at Yelp and Google review but would really like more substantial data with more meat on the bone. The review sites she has looked at are mostly personal opinions and she is looking for more fact based data.</p>
 
 	</body>
 </html>
