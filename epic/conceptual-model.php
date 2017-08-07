0733cef46e96d0a4be4b96978469bd731f192ed4
Steve Stone <stoneflynm@gmail.com>
2017-08-07 08:48:10 -0600

    added conceptual model

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -1,2 +1,34 @@
-<?php
-//the conceptual model will go in here
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Conceptual Model</title>
+	</head>
+	<body>
+		<h1>Conceptual Model</h1>
+		<h3>Restaurant</h3>
+		<ul>
+			<li>restaurantId(primaryKey)</li>
+			<li>restaurantName</li>
+			<li>restaurantFacilityKey</li>
+			<li>restaurantCategory</li>
+			<li>restaurantAddress</li>
+			<li>restaurantPhoneNumber</li>
+			<li>restaurantGoogleId</li>
+		</ul>
+		<h3>Violation</h3>
+		<ul>
+			<li>violationId(primaryKey)</li>
+			<li>violationCode</li>
+			<li>violationCodeDescription</li>
+		</ul>
+		<h3>RestaurantViolation</h3>
+		<ul>
+			<li>restaurantViolationId(primaryKey)</li>
+			<li>restaurantViolationRestaurantId</li>
+			<li>restaurantViolationViolationId</li>
+			<li>restaurantViolationMemo</li>
+			<li>restaurantViolationResult</li>
+		</ul>
+	</body>
+</html>
