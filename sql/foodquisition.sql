07d825947905aae8847232c3e66c1ee708fb6ec7
Danielle Branch <dbranch82@gmail.com>
2017-08-08 11:52:50 -0600

    removed unneeded unique tags

--- sql/foodquisition.sql
+++ sql/foodquisition.sql
@@ -1,4 +1,5 @@
 CREATE TABLE restaurant (
+	restaurantId INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	restaurantAddress1 VARCHAR(128) NOT NULL,
 	restaurantAddress2 VARCHAR(128),
 	restaurantCity VARCHAR(64) NOT NULL,
@@ -6,40 +7,36 @@ CREATE TABLE restaurant (
 	restaurantState CHAR (2) NOT NULL,
 	restaurantFacilityKey CHAR(7) NOT NULL,
 	restaurantGoogleId VARCHAR(128),
-	restaurantId INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	restaurantName VARCHAR(64) NOT NULL,
 	restaurantPhoneNumber VARCHAR(32),
 	restaurantType VARCHAR(64),
 	UNIQUE (restaurantFacilityKey),
-	UNIQUE (restaurantGoogleId),
+	INDEX (restaurantGoogleId),
 	PRIMARY KEY (restaurantId)
 );
 
 CREATE TABLE category(
 	categoryId INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	categoryName VARCHAR(32),
-	UNIQUE (categoryId),
 	PRIMARY KEY (categoryId)
 );
 
 CREATE TABLE violation(
+	violationId INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	violationCategoryId INT UNSIGNED NOT NULL,
 	violationCode VARCHAR(8) NOT NULL,
-	violationCodeDescription VARCHAR(256) NOT NULL,
-	violationId INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	UNIQUE (violationId),
+	violationCodeDescription VARCHAR(255) NOT NULL,
 	FOREIGN KEY (violationCategoryId) REFERENCES category(categoryId),
 	PRIMARY KEY (violationId)
 );
 
-CREATE TABLE restaurantviolation(
-	restaurantViolationDate DATE NOT NULL,
+CREATE TABLE restaurantViolation(
 	restaurantViolationId INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	restaurantViolationMemo VARCHAR(256),
 	restaurantViolationRestaurantId INT UNSIGNED NOT NULL,
-	restaurantViolationResults VARCHAR(32) NOT NULL,
 	restaurantViolationViolationId INT UNSIGNED NOT NULL,
-	UNIQUE (restaurantViolationId),
+	restaurantViolationDate DATE NOT NULL,
+	restaurantViolationMemo VARCHAR(255),
+	restaurantViolationResults VARCHAR(32) NOT NULL,
 	FOREIGN KEY (restaurantViolationRestaurantId) REFERENCES restaurant(restaurantId),
 	FOREIGN KEY (restaurantViolationViolationId) REFERENCES violation(violationId),
 	PRIMARY KEY (restaurantViolationId)
