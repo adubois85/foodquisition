4ba21b4e6144af87aa7dcbf2742d296983ab1923
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-08-09 08:49:10 -0600

    starting classes

---
+++ php/class/RestaurantViolation.php
@@ -0,0 +1,21 @@
+<?php
+namespace Edu\CNM\foodquisition;
+
+require_once("autoloader.php");
+/*item class*/
+
+/**
+ * Create RestaurantViolation class
+ *
+ * This creates a class
+ *
+ * @author Dannielle Bojorquz <dbojorquez@cnm.edu>
+ * @version 1.0.0
+ **/
+class RestaurantViolation implements \JsonSerializable {
+	/**
+	 * id for this RestaurantViolation;this is the primary key
+	 * @var int $restaurantViolation
+	 */
+}
+
