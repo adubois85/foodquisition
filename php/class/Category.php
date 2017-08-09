0c209f8930cec32f39136e64edc230a32d988eff
Steve Stone <stoneflynm@gmail.com>
2017-08-09 09:23:51 -0600

    started category class

--- php/class/Category.php
+++ php/class/Category.php
@@ -1,2 +1,29 @@
 <?php
 namespace Edu\Cnm\Foodquisition;
+
+require_once("autoload.php");
+
+/**
+ * @author Steve Stone <stoneflynm@gmail.com>
+ * @version 1.0.0
+**/
+
+class Category implements \JsonSerializable {
+	/**
+	 * id for this category; this is the primary key
+	 * @var int $categoryId
+	 **/
+	private $categoryId;
+	/**
+	 * name of the category
+	 * @var string $categoryName
+	 */
+	private $categoryName;
+
+	/**
+	 * constructor for this category
+	 *
+	 * @param int|null $newCategoryId id of this category or null if a new category
+	 * @param
+	 */
+}
