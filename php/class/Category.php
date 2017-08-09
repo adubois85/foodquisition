14490ab94501ed843a04de4c0b56576b69e5709d
Steve Stone <stoneflynm@gmail.com>
2017-08-09 09:58:27 -0600

    finished constructor

--- php/class/Category.php
+++ php/class/Category.php
@@ -24,6 +24,28 @@ class Category implements \JsonSerializable {
 	 * constructor for this category
 	 *
 	 * @param int|null $newCategoryId id of this category or null if a new category
-	 * @param
-	 */
+	 * @param string $newCategoryName string containing name of category
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
+	public function __construct(?int $newCategoryId, string $newCategoryName) {
+		try {
+			$this->setCategoryId($newCategoryId);
+			$this->setCategoryName($newCategoryName);
+		}
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * accessor method for category id
+	 *
+	 * @return int|null value fo category id
+	 **/
+	
 }
