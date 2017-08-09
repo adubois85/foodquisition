4b5a69da378d1a96382f2d7385002e6186af684c
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-09 08:49:02 -0600

    added restaurantId variable, corrected namespace file https://app.asana.com/0/395186218565213/404244516371085

--- php/class/Restaurant.php
+++ php/class/Restaurant.php
@@ -1,3 +1,19 @@
 <?php
-namespace Edu\CNM\Foodquisition;
+namespace Edu\Cnm\Foodquisition;
 
+/**
+ * @author Alexander DuBois <adubois@alumni.uci.edu>
+ * @version 0.1
+ */
+class Restaurant implements \JsonSerializable {
+
+	/**
+	 * ID for this restaurant; will be a unique, auto-incrementing integer.
+	 * This is the primary key.
+	 * @var int $restaurantId
+	 */
+	private $restuaruantId;
+
+
+
+}
