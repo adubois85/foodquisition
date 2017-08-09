2f3c5f897ddc0695af10a779092c50ba2396e5b9
Danielle Branch <dbranch82@gmail.com>
2017-08-09 09:00:04 -0600

    Started class and renamed autoload.php

--- php/class/Violation.php
+++ php/class/Violation.php
@@ -1,2 +1,19 @@
 <?php
 namespace Edu\Cnm\Foodquisition;
+
+require_once("autoload.php");
+
+/**
+ * Small Cross Section of a Violation
+ *
+
+ * @author Danielle Branch <dbranch82@gmail.com>
+ * @version 4.0.1
+ **/
+class Violation implements \JsonSerializable {
+	use ValidateDate;
+	/**
+	 * id for this Violation; this is the primary key
+	 * @var int $ViolationId
+	 **/
+
