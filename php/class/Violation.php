50fed7af3f41f90cb5e0ce80703278ca79c59c83
Danielle Branch <dbranch82@gmail.com>
2017-08-09 10:12:13 -0600

    added parameters

--- php/class/Violation.php
+++ php/class/Violation.php
@@ -27,12 +27,15 @@ class Violation implements \JsonSerializable {
 	private $violationCode;
 	/**
 	 *
-	 *
 	 * @var string $violationCodeDescription
 	 */
 	private $violationCodeDescription;
 	/**
 	 * Constructor for Violation
+	 * @param int|null $newViolationId id of this Violation or null if a new Violation
+	 * @param int $newViolationCategoryId id of the Category for this Violation
+	 * @param string $newViolationCode string that references actual Violation
+	 * @param string $newViolationCodeDescription string that actually describes code violation
 	 *
 	 *
 	 */
