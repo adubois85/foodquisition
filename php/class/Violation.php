2cf216d9dde337395c8b1acf742105e17a22105d
Danielle Branch <dbranch82@gmail.com>
2017-08-09 10:23:41 -0600

    added public function

--- php/class/Violation.php
+++ php/class/Violation.php
@@ -36,8 +36,25 @@ class Violation implements \JsonSerializable {
 	 * @param int $newViolationCategoryId id of the Category for this Violation
 	 * @param string $newViolationCode string that references actual Violation
 	 * @param string $newViolationCodeDescription string that actually describes code violation
-	 *
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
 	 *
 	 */
+	public function __construct(?int $newViolationId, int $newViolationCategoryId, string $newViolationCode, string $newViolationCodeDescription) {
+		try {
+			$this->setViolationId($newViolationId);
+			$this->setViolationCategoryId($newViolationCategoryId);
+			$this->setViolationCode($newViolationCode);
+			$this->setViolationCodeDescription($newViolationCodeDescription);
+		}
+			//determine what exception type was thrown
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
 }
 
