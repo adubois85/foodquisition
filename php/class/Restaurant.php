3eb8f700db6715d9d91774d786480628cc40505b
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-09 09:30:52 -0600

    added first half of class states file https://app.asana.com/0/395186218565213/404244516371085

--- php/class/Restaurant.php
+++ php/class/Restaurant.php
@@ -15,5 +15,44 @@ class Restaurant implements \JsonSerializable {
 	private $restuaruantId;
 
 
+	/**
+	 * Per USPS standards, the address 1 line of the address (street number, name, direction, etc.) for this facility.
+	 * Cannot be NULL
+	 * @var string $restaurantAddress1
+	 */
+	private $restaurantAddress1;
 
-}
+	/**
+	 * Per USPS standards, the address 2 line of the address (Typically delivery comments such as "leave at door") for this facility.
+	 * @var string $restaurantAddress2
+	 */
+	private $restaurantAddress2;
+
+	/**
+	 * Per USPS standards, the city where this facilty is located.
+	 * Cannot be NULL
+	 * @var string $restaurantCity
+	 */
+	private $restaurantCity;
+
+	/**
+	 * The unique identifying number given to this facility by the City of Albuquerque
+	 * Cannot be NULL
+	 * @var string $restaurantFacilityKey
+	 */
+	private $restaurantFacilityKey;
+
+	/**
+	 * The ID number given by Google for this restaurant to facilitate pulling information from Google APIs.  In theory it should be unique, but that's not guaranteed.
+	 * Cannot be NULL
+	 * @var string $restaurantGoogleId
+	 */
+	private $restaurantGoogleId;
+
+	/**
+	 * The name of this facility.
+	 * Cannot be NULL
+	 * @var string $restaurantName
+	 */
+	private $restaurantName;
+
