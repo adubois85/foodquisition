0b3dc367620059df363c65f0b6252011c24a59fd
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-08-09 11:00:20 -0600

    more class material

--- php/class/RestaurantViolation.php
+++ php/class/RestaurantViolation.php
@@ -13,9 +13,36 @@ require_once("autoloader.php");
  * @version 1.0.0
  **/
 class RestaurantViolation implements \JsonSerializable{
+	use ValidateDate;
 	/**
-	 * id for this RestaurantViolation;this is the primary key
+	 * id for this RestaurantViolation; this is the primary key
 	 * @var int $restaurantViolation
-	 */
+	 **/
+	private $restaurantViolationId;
+	/**
+	 * id of the restaurant that has the violation; this is a foreign key
+	 * @var int $restaurantViolationRestaurantId
+	 **/
+	private $restaurantViolationRestaurantId;
+	/**
+	 * id of the violation in the Restaurant; this is a foreign key
+	 * @var $restaurantViolationViolationId
+	 **/
+	private $restaurantViolationViolationId;
+	/**
+	 * date the violation was on
+	 * @var \Date $restaurantViolationDate
+	 **/
+	private $restaurantViolationDate;
+	/**
+	 * memo content of the violation
+	 * @var string $restaurantViolationMemo
+	 **/
+	private $restaurantViolationMemo
+	/**
+	 *
+	 **/
+
+
 }
 
