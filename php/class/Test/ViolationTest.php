73cf7cb12fe4d256fa9303c1e2a7850d865c4bf0
Danielle Branch <dbranch82@gmail.com>
2017-08-11 10:18:48 -0600

    https://app.asana.com/0/404235026164260/404249091927835  test inserting a Violation

--- php/class/Test/ViolationTest.php
+++ php/class/Test/ViolationTest.php
@@ -40,30 +40,41 @@ class ViolationTest extends FoodquisitionTest {
 	 * @var string $VALID_VIOLATIONCODEDESCRIPTION
 	 */
 	protected $VALID_VIOLATIONCODESCRIPTION;
+
 	/**
 	 * test inserting a valid Violation and verify  that the actual mySQL data matches
 	 */
-	public function testInsertValidViolation() : void {
+	public function testInsertValidViolation(): void {
 		// count the number of the rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("violation");
+	}
 		/**
 		 *create dependent objects before running each test
 		 **/
-		public final function setUP() : void {
+		public final function setUP(): void {
 			//run the default setUp() method first
 			parent::setUp();
-			$this->category = new Category(null, null,)
+			$this->category = new Category(null, null,);
+				$this->category->insert($this->getPDO());
+
 		}
 
 		//create a new Violation and insert to mySQL
-		$violation = new Violation(null, $this->Violation->getViolationId(), $this->VALID_VIOLATIONCATEGORYID, $this->VALID_VIOLATIONCODE, $this->VALID_VIOLATIONCODEDESCRIPTION);
-		$violation->insert($this->getPDO());
+		public function testInsertValidViolation(): void {
+			$violation = new Violation(null, $this->Violation->getViolationId(), $this->VALID_VIOLATIONCATEGORYID, $this->VALID_VIOLATIONCODE, $this->VALID_VIOLATIONCODEDESCRIPTION);
+			$violation->insert($this->getPDO());
 
-		// grab the data from mySQL and enforce the fields match our expectations
-		$pdoViolation = Violation::getViolationByViolationId($this->getPDO(), $violation->getViolationId());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("violation"));
-		$this->assertEquals($pdoViolation->getViolationCategoryId(), $this->getViolationCategoryId());
-		$this->assertEquals($pdoViolation->getViolationCode(), $this->getViolationCode);
-		$this->assertEquals($pdoViolation->getViolationCodeDescription(), $this->get);
+			// grab the data from mySQL and enforce the fields match our expectations
+			$pdoViolation = Violation::getViolationByViolationId($this->getPDO(), $violation->getViolationId());
+			$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("violation"));
+			$this->assertEquals($pdoViolation->getViolationCategoryId(), $this->category->getViolationCategoryId());
+			$this->assertEquals($pdoViolation->getViolationCode(), $this->getViolationCode);
+			$this->assertEquals($pdoViolation->getViolationCodeDescription(), $this->get);
+		}
+		/**
+		 * test inserting a Violation that already exists
+		 *
+		 *
+		 */
 	}
 }
