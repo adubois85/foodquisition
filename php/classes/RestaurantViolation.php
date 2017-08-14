61eb991df60ab425cf664fd4db81c228f99690b8
Danielle Branch <dbranch82@gmail.com>
2017-08-14 16:27:40 -0600

    https://app.asana.com/0/404235026164260/404249091927836 Created Validate date file

--- php/classes/RestaurantViolation.php
+++ php/classes/RestaurantViolation.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Edu\Cnm\Foodquisition;
 
 require_once("autoload.php");
@@ -9,13 +10,27 @@ require_once("autoload.php");
  * @author Dinn Bojorquez
  * @version 1.0
  */
-
-class RestaurantViolation implements \JsonSerializable{
+class RestaurantViolation implements \JsonSerializable {
 	use ValidateDate;
 	/**
 	 *id for restaurant violation
 	 * @var int $restaurantViolationId
 	 *
-	 */
+	 **/
+	private $restaurantViolationId;
+	/**
+	 * id of the restaurant violation restaurant
+	 * @var int $restaurantViolationRestaurantId
+	 **/
+	private $restaurantViolationRestaurantId;
+	/**
+	 * id of the restaurant violation violation
+	 * @var int $restaurantViolationViolationId
+	 **/
+	private $restaurantViolationViolationId;
+	/**
+	 * date of restaurant violation
+	 * @var \DateTime $restaurantViolationDate
+	 **/
 
 }
