5c620efa43cbf9fb2473f2795ebb9aba1bd274aa
Steve Stone <stoneflynm@gmail.com>
2017-08-14 16:10:59 -0600

    finished inserting, editing, updating Category test https://app.asana.com/0/404235026164260/404249091927837

--- php/classes/Test/CategoryTest.php
+++ php/classes/Test/CategoryTest.php
@@ -34,6 +34,44 @@ class CategoryTest extends FoodquisitionTest {
 
 		//create a new Category and insert into mqSQL
 		$category = new Category(null, $this->VALID_CATEGORYNAME);
+		$category->insert($this->getPDO());
 
+		//grab category back from mySQL and verify all fields match
+		$pdoCategory = Category::getCategoryByCategoryId($this->getPDO(), $category->getCategoryId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("category"));
+		$this->assertEquals($pdoCategory->getCategoryName(), $this->VALID_CATEGORYNAME);
 	}
+
+	/**
+	 * test inserting a Category that all ready exists
+	 *
+	 * @expectedException \PDOException
+	 **/
+	public function testInsertInvalidCategory() : void {
+		// create a category with a non null categoryId and watch it fail
+		$category = new Category(FoodquisitionTest::INVALID_KEY, $this->VALID_CATEGORYNAME);
+		$category->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a Category, editing it, and then updating it
+	 */
+	public function testUpdateValidCategory() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("category");
+
+		//create a new Category and insert into mySQL
+		$category = new Category(null, $this->VALID_CATEGORYNAME);
+		$category->insert($this->getPDO());
+
+		// edit the Category and update it in mySQL
+		$category->setCategoryName($this->VALID_CATEGORYNAME);
+		$category->update($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoCategory = Category::getCategoryByCategoryId($this->getPDO(), $category->getCategoryId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("category"));
+		$this->assertEquals($pdoCategory->getCategoryName(), $this->VALID_CATEGORYNAME);
+	}
+
 }
