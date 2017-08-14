f2e065639996b7c059f267a38873a19e0c909172
Danielle Branch <dbranch82@gmail.com>
2017-08-14 16:45:18 -0600

    https://app.asana.com/0/404235026164260/404249091927836 Created Constructor for restaurantViolation

--- php/classes/RestaurantViolation.php
+++ php/classes/RestaurantViolation.php
@@ -32,5 +32,34 @@ class RestaurantViolation implements \JsonSerializable {
 	 * date of restaurant violation
 	 * @var \DateTime $restaurantViolationDate
 	 **/
+	private $restaurantViolationDate;
+	/**
+	 * memo for restaurant violation
+	 * @varchar string $restaurantViolationMemo
+	 *
+	 **/
+	private $restaurantViolationMemo;
+	/**
+	 *
+	 * Results for restaurant violation
+	 * @varchar string $restaurant
+	 */
+	private $restaurantViolationResults;
+
+	/**
+	 * constructor for this restaurantViolation
+	 *
+	 * @param int|null $newRestaurantViolationId of this restaurantViolation or null if a new restaurantViolation
+	 * @param int $newRestaurantViolationRestaurantId of the restaurantId that caused the restaurant violation
+	 * @param int $restaurantViolationViolation of the restaurant violation violation
+	 * @param \DateTime|string|null $newRestaurantViolationDate date and time restaurant violation was sent or null if set to current date and time
+	 * @param string $restaurantViolationMemo string containing inspector notes
+	 * @param string $restaurantViolationResults sting containing results of inspection
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
 
 }
