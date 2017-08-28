267278aa349a4779ca3fa8c7fef50a4f156d962a
Danielle Branch <dbranch82@gmail.com>
2017-08-28 13:30:12 -0600

    about to put in name space

--- php/classes/data-downloader.php
+++ php/classes/data-downloader.php
@@ -83,22 +83,93 @@ class DataDownloader {
 		 */
 		public static function readBusinessesCSV($urlBegin, $urlEnd) {
 			$urls = glob("urlBegin*$urlEnd");
-			if(count($urls)>0) {
+			if(count($urls) > 0) {
 				$url = $urls[0];
 			}
 			$context = stream_context_create(array("http" => array("ignore_errors" => true, "method" => "GET")));
 
-			try{
+			try {
 				$pdo = connectToEncryptedMySQL("/etc/apache2/mysql/foodquisition.ini");
 
 				if(($fd = @fopen($url, "rb", false, $context)) !== false) {
 					fgetcsv($fd, 0, ",");
 					while((($data = fgetcsv($fd, 0, ",")) !== false) && feof($fd) === false) {
-						//line 258//
+						$restaurantId = null;
+						$googleId = "";
+						$facilityKey = $data[1];
+						$name = $data[2];
+						$address = $data[3];
+						$phone = $data;
+
+						//Convert everything to UTF - 8
+
+						$facilityKey = mb_convert_encoding($facilityKey, "UTF-8", "UTF-16");
+						$name = mb_convert_encoding($name, "UTF-8", "UTF-16");
+						$name = str_replace("\"", "", $name);
+						$address = mb_convert_encoding($address, "UTF-8", "UTF-16");
+						$phone = mb_convert_encoding($phone, "UTF-8", "UTF-16");
+
+						try {
+							$restaurant = new Restaurant($restaurantId, $googleId, $facilityKey, $name, $address, $phone);
+							$restaurant->insert($pdo);
+						} catch(PDOException $pdoException) {
+							$sqlStateCode = "23000";
+
+							$errorInfo = $pdoException->errorInfo;
+							if($errorInfo[0] === $sqlStateCode) {
+								echo "<p>Duplicate</p>";
+							} else {
+								throw(new PDOException($pdoException->getMessage(), 0, $pdoException));
+							}
+						} catch(Exception $exception) {
+							throw(new Exception($exception->getMessage(), 0, $pdoException));
+						}
+
+					}
+					try {
+						$category = new Category($categoryName, $googleId, $facilityKey, $name, $address, $phone);
+						$category->insert($pdo);
+					} catch(PDOException $pdoException) {
+						$sqlStateCode = "23000";
+
+						$errorInfo = $pdoException->errorInfo;
+						if($errorInfo[0] === $sqlStateCode) {
+							echo "<p>Duplicate</p>";
+						} else {
+							throw(new PDOException($pdoException->getMessage(), 0, $pdoException));
+						}
+					} catch(Exception $exception) {
+						throw(new Exception($exception->getMessage(), 0, $pdoException));
+					}
+					try {
+						$violation = new Violation($violationId, $googleId, $facilityKey, $name, $address, $phone);
+						$violation->insert($pdo);
+					} catch(PDOException $pdoException) {
+						$sqlStateCode = "23000";
+
+						$errorInfo = $pdoException->errorInfo;
+						if($errorInfo[0] === $sqlStateCode) {
+							echo "<p>Duplicate</p>";
+						} else {
+							throw(new PDOException($pdoException->getMessage(), 0, $pdoException));
+						}
+					} catch(Exception $exception) {
+						throw(new Exception($exception->getMessage(), 0, $pdoException));
+					}
+					try {
+						$restaurantViolation = new RestaurantViolation($restaurantViolationId, $googleId, $facilityKey, $name, $address, $phone);
+						$restaurantViolation->insert($pdo);
+					} catch(PDOException $pdoException) {
+						$sqlStateCode = "23000";
+
+						$errorInfo = $pdoException->errorInfo;
+						if($errorInfo[0] === $sqlStateCode) {
+							echo "<p>Duplicate</p>";
+						} else {
+							throw(new PDOException($pdoException->getMessage(), 0, $pdoException));
+						}
+					} catch(Exception $exception) {
+						throw(new Exception($exception->getMessage(), 0, $pdoException));
 					}
 				}
 			}
-		}
-		}
-	}
-}
