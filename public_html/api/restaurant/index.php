0bf2461b59c8aa0b3d282dc2f5f61036b8b529b9
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-28 16:09:08 -0600

    Created INSERT statements for category https://app.asana.com/0/404235026164260/413053450778113

---
+++ public_html/api/restaurant/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once(dirname(__DIR__,2) . "/php/classes/autoload.php");
+require_once (dirname(__DIR__,2) . "/php/lib/xsrf.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\Foodquisition\Restaurant;
---
+++ sql/seed-data.sql
@@ -0,0 +1,50 @@
+INSERT INTO category (categoryId, categoryName)
+VALUES (1, "SUPERVISION / EMPLOYEE HEALTH");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (2, "GOOD HYGIENIC PRACTICES");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (3, "PREVENTING CONTAMINATION BY HANDS");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (4, "APPROVED SOURCES");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (5, "PROTECTION FROM CONTAMINATION");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (6, "POTENTIALLY HAZARDOUS FOOD");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (7, "CONSUMER ADVISORY");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (8, "HIGHLY SUSCEPTIBLE POPULATIONS");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (9, "CHEMICALS");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (10, "CONFORMS TO APPROVED PROCEDURES");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (11, "SAFE FOOD AND WATER");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (12, "FOOD TEMPERATURE CONTROL");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (13, "FOOD IDENTIFICATION");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (14, "PREVENTION OF FOOD CONTAMINATION");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (15, "PROPER USE OF UTENSILS");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (16, "UTENSILS, EQUIPMENT AND VENDING");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (17, "PHYSICAL FACILITIES");
