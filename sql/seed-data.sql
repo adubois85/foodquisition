4994150853210ba2df9495a00a77755dde9ee9c5
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-29 00:28:23 -0600

    refined category names in INSERT statements https://app.asana.com/0/404235026164260/413053450778113

--- sql/seed-data.sql
+++ sql/seed-data.sql
@@ -1,50 +1,53 @@
 INSERT INTO category (categoryId, categoryName)
-VALUES (1, "SUPERVISION / EMPLOYEE HEALTH");
+VALUES (1, "No manager present/knowledgable");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (2, "GOOD HYGIENIC PRACTICES");
+VALUES (2, "Employees working while sick");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (3, "PREVENTING CONTAMINATION BY HANDS");
+VALUES (3, "Bad hygiene practices");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (4, "APPROVED SOURCES");
+VALUES (4, "Potential contamination by hands");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (5, "PROTECTION FROM CONTAMINATION");
+VALUES (5, "Potential bad source/unsafe food");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (6, "POTENTIALLY HAZARDOUS FOOD");
+VALUES (6, "Potential contamination");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (7, "CONSUMER ADVISORY");
+VALUES (7, "Improper cook time, temps, dates");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (8, "HIGHLY SUSCEPTIBLE POPULATIONS");
+VALUES (8, "Consumer advisories not posted");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (9, "CHEMICALS");
+VALUES (9, "Served at-risk pop unsafe foods");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (10, "CONFORMS TO APPROVED PROCEDURES");
+VALUES (10, "Unsafe use of chemicals/additive");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (11, "SAFE FOOD AND WATER");
+VALUES (11, "Used unapproved food processing");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (12, "FOOD TEMPERATURE CONTROL");
+VALUES (12, "Unsafe eggs/water/processing");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (13, "FOOD IDENTIFICATION");
+VALUES (13, "Improper thawing or holding");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (14, "PREVENTION OF FOOD CONTAMINATION");
+VALUES (14, "Food improperly labeled");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (15, "PROPER USE OF UTENSILS");
+VALUES (15, "Potential infestation or contamination");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (16, "UTENSILS, EQUIPMENT AND VENDING");
+VALUES (16, "Improper use of utensils");
 
 INSERT INTO category (categoryId, categoryName)
-VALUES (17, "PHYSICAL FACILITIES");
+VALUES (17, "Unclean surfaces / facilities");
+
+INSERT INTO category (categoryId, categoryName)
+VALUES (18, "Facilities not up to code");
