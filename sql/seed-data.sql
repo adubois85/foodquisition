5c767dc234ec4b6f5cfe754242a882aced4ad75b
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-29 09:44:24 -0600

    Created framework for violation INSERT statements seed data https://app.asana.com/0/404235026164260/413053450778113

--- sql/seed-data.sql
+++ sql/seed-data.sql
@@ -51,3 +51,171 @@ VALUES (17, "Unclean surfaces / facilities");
 
 INSERT INTO category (categoryId, categoryName)
 VALUES (18, "Facilities not up to code");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (1, 1, "1", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (2, 2, "2", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (3, 2, "3", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (4, 3, "4", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (5, 3, "5", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (6, 4, "6", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (7, 4, "7", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (8, 4, "8", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (9, 5, "9", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (10, 5, "10", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (11, 5, "11", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (12, 5, "12", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (13, 6, "13", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (14, 6, "14", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (15, 6, "15", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (16, 7, "16", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (17, 7, "17", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (18, 7, "18", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (19, 7, "19", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (20, 7, "20", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (21, 7, "21", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (22, 7, "22", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (23, 8, "23", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (24, 9, "24", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (25, 10, "25", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (26, 10, "26", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (27, 11, "27", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (28, 12, "28", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (29, 12, "29", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (30, 12, "30", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (31, 13, "31", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (32, 13, "32", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (33, 13, "33", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (34, 13, "34", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (35, 14, "35", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (36, 15, "36", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (37, 15, "37", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (38, 15, "38", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (39, 15, "39", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (40, 15, "40", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (41, 16, "41", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (42, 16, "42", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (43, 16, "43", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (44, 16, "44", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (45, 17, "45", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (46, 17, "46", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (47, 17, "47", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (48, 18, "48", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (49, 18, "49", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (50, 18, "50", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (51, 18, "51", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (52, 18, "52", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (53, 18, "53", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (54, 18, "54", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (55, 18, "55", "");
+
+INSERT INTO violation (violationId, violationCategoryId, violationCode, violationCodeDescription)
+VALUES (56, 18, "56", "");
