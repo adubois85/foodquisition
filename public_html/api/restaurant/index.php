a9e14fc6bc579db7807984bf3500d765ad57914e
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-31 16:56:41 -0600

    added variable for Google Places API key, from encrypted ini https://app.asana.com/0/404235026164260/413053450778113

--- public_html/api/restaurant/index.php
+++ public_html/api/restaurant/index.php
@@ -2,6 +2,12 @@
 require_once(dirname(__DIR__,2) . "/php/classes/autoload.php");
 require_once (dirname(__DIR__,2) . "/php/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+$config = readConfig("/etc/apache2/capstone-mysql/foodquisition.ini");
+
+// $config["google"] now exists
+
+
 use Edu\Cnm\Foodquisition\Restaurant;
 
 /*
