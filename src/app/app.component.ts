9b7a43719b6d005144b159b1dd8f1d82fc054255
Steve Stone <stoneflynm@gmail.com>
2017-09-04 15:02:41 -0600

    added app component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
 	selector: "foodquisition",
-	templateUrl: "./templates/foodquisition.php"
+	templateUrl: "./templates/foodquisition-app.php"
 })
 
 export class AppComponent {}
