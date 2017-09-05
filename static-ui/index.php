59b21c9436456a72d9477079c8a3fecc634bc78b
Steve Stone <stoneflynm@gmail.com>
2017-09-04 22:03:31 -0600

    added static index.php

---
+++ static-ui/index.php
@@ -0,0 +1,40 @@
+<?php require_once("lib/head-utils.php");?>
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- insert header and navbar -->
+<!--		--><?php //require_once("lib/header.php");?>
+
+		<main class="search">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="jumbotron">
+							<div class="panel panel-default">
+								<div class="panel-body lead">
+									<form class="">
+										<div class="form-group">
+											<label class="sr-only">Search</label>
+											<p class="">Foodquisition: Where Can We Eat Tonight</p>
+										</div>
+										<div class="form-group">
+											<label for="restaurantSearch" class="sr-only">Restaurant</label>
+											<div class="col-md-10">
+											<input type="text" class="form-control" id="restaurantSearch" placeholder="Restaurant">
+											</div>
+										</div>
+										<button type="submit" class="btn btn-primary">Sumbit</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div><!--/.sfooter-content-->
+
+	<!-- insert footer -->
+<!--	--><?php //require_once("lib/footer.php");?>
+</body>
+</html>
