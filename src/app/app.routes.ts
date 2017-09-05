0df0e7d3cef054300ac72fb7c390a128467c017e
Steve Stone <stoneflynm@gmail.com>
2017-09-05 15:16:18 -0600

    updated gitignore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,19 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
-import {AboutComponent} from "./components/about.component";
+import {CardComponent} from "./components/card.component";
 import {PostsComponent} from "./components/posts.component";
-import {NavbarComponent} from "./components/navbar.component";
-import {FooterComponent} from "./components/footer.component";
+
 import {PostService} from "./services/post-service";
 
 export const allAppComponents = [
 	HomeComponent,
-	AboutComponent,
-	PostsComponent,
-	NavbarComponent,
-	FooterComponent
+	CardComponent,
+	PostsComponent
 ];
 
 export const routes: Routes = [
 	{path: "posts", component:PostsComponent},
-	{path: "about", component:AboutComponent},
+	{path: "card", component:CardComponent},
 	{path: "", component: HomeComponent},
 	{path: "**", redirectTo: ""}
 ];
