f9a27125c0fe52b1941f58f746424ea4e882233d
Dannielle Bojorquez <dbojorquez@cnm.edu>
2017-09-05 16:18:53 -0600

    added result.php https://app.asana.com/0/404235026164260/404249091927836

---
+++ static-ui/result.php
@@ -0,0 +1,27 @@
+<?php require_once("lib/head-utils.php"); ?>
+<body class="card">
+	<div class="panel panel-default">
+		<div class="panel-body row" >
+			<div class="col-md-4">
+				<img class="img-responsive" src="images/taco.jpg">
+			</div>
+			<div class="col-md-6">
+				<h1>Taco Bell</h1>
+				<p>1524 West Elm st. SW ALbuquerque,NM 87104</p> <br>
+				<p>Get all of your yummy food needs here</p>
+			</div>
+			<div class="col-md-2">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<P>8/10/2016<br> Last Inspection Date</P>
+			</div>
+		</div>
+		<hr>
+		<div class="panel-body row">
+			<div class="center">
+				<h2>Violations</h2>
+				<p class=""><i class="fa fa-hand-spock-o" aria-hidden="true"></i> Did not witness washing of hands</p>
+				<p><i class="fa fa-cutlery" aria-hidden="true"></i> Dishes not properly washed</p>
+				<p><img src="https://png.icons8.com/hamster/ios7/32" title="Hamster" width="32" height="32"> Yup there were Mice</p>
+			</div>
+		</div>
+	</div>
