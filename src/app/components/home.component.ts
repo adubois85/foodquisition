a0dab5ff1b51d8da5f344537d40cf4951d78fe70
Steve Stone <stoneflynm@gmail.com>
2017-09-04 18:32:15 -0600

    added home component

---
+++ src/app/components/home.component.ts
@@ -0,0 +1,9 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs";
+
+@Component({
+	templateUrl: "./templates/home.php"
+})
+
+export class HomeComponent {}
