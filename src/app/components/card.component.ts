16eaaae228798cbe2b2f7df7e87306f0a57a94ed
Danielle Branch <dbranch82@gmail.com>
2017-09-07 15:31:32 -0600

    changed class name

--- src/app/components/card.component.ts
+++ src/app/components/card.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from "@angular/core";
-
 import {ActivatedRoute, Params} from "@angular/router";
 import {Observable} from "rxjs";
+import {RestaurantService} from "../services/restaurant.service"
+import {Restaurant} from "../classes/restaurant"
+import {Status} from "../classes/status";
+import {RestaurantViolation} from "../classes/restaurantViolation";
 
 @Component({
 	templateUrl: "./templates/card.html"
