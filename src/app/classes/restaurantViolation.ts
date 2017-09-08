93d940663bb45fe47d9e197985d1e5ce06c7f2e5
Danielle Branch <dbranch82@gmail.com>
2017-09-08 13:35:33 -0600

    added pics

--- src/app/classes/restaurantViolation.ts
+++ src/app/classes/restaurantViolation.ts
@@ -1,3 +1,3 @@
 export class RestaurantViolation {
-	constructor(public restuarantViolationId: number, restaurantViolationRestaurantId: number, restaurantViolationViolationId: number, restaurantViolationCompliance: string, restaurantViolationDate: Date, restaurantViolationMemo: string, restaurantViolationResults: string) {}
+	constructor(public restaurantViolationId: number, restaurantViolationRestaurantId: number, restaurantViolationViolationId: number, restaurantViolationCompliance: string, restaurantViolationDate: Date, restaurantViolationMemo: string, restaurantViolationResults: string) {}
 }
--- src/app/services/restaurantViolation.service.ts
+++ src/app/services/restaurantViolation.service.ts
@@ -5,6 +5,7 @@ import {BaseService} from "./base.service";
 import {RestaurantViolation} from "../classes/RestaurantViolation";
 import {Status} from "../classes/status";
 
+
 @Injectable()
 export class RestaurantViolationService extends BaseService {
 	constructor(protected http: Http) {
@@ -13,22 +14,15 @@ export class RestaurantViolationService extends BaseService {
 
 	private restaurantViolationUrl = "apis/restaurantViolation/";
 
-	getAllRestaurantViolations() : Observable<RestaurantViolation[]> {
-		return(this.http.get(this.restaurantViolationUrl)
+	getAllRestaurantViolations(): Observable<RestaurantViolation[]> {
+		return (this.http.get(this.restaurantViolationUrl)
 			.map(this.extractData)
 			.catch(this.handleError));
 	}
 
-	getRestaurantViolationByRestaurantViolationId(restaurantViolationId : number) : Observable<RestaurantViolation[]> {
-		return(this.http.get(this.restaurantViolationUrl + restaurantViolationId)
+	getRestaurantViolationByRestaurantViolationId(restaurantViolationId: number): Observable<RestaurantViolation[]> {
+		return (this.http.get(this.restaurantViolationUrl + restaurantViolationId)
 			.map(this.extractData)
 			.catch(this.handleError));
 	}
-	getRestaurantViolationCompliance(restaurantViolationCompliance : string) : Observable<RestaurantViolationCompliance[]> {
-		return(this.http.get(this.getRestaurantViolationCompliance + restaurantViolationCompliance)
-			.map(this.extractData)
-			.catch(this.handleError));
-	}
-
-
 }
