10fc05be24a9defa919feef6e448aeedfbdcdadc
Alexander DuBois <adubois@alumni.uci.edu>
2017-09-11 09:43:23 -0600

    Replaced strreplace with urlencode; copied changes to array version https://app.asana.com/0/404235026164260/413053450778113

--- php/lib/google-places.php
+++ php/lib/google-places.php
@@ -28,7 +28,7 @@ function googlePlacesSingle($restaurant, $googleId) {
 	// if a Restaurant doesn't have a Google ID, then try to find one and add it
 	if($googleId === null) {
 		// we need to be specific when searching Google's database so we don't get similarly named places back
-		$query = str_replace(" ", "+", $restaurant->getRestaurantName() . "+" . $restaurant->getRestaurantAddress1() . "+" . $restaurant->getRestaurantCity());
+		$query = urlencode(($restaurant->getRestaurantName() . "+" . $restaurant->getRestaurantAddress1() . "+" . $restaurant->getRestaurantCity()));
 		$response = json_decode(($googlePlaces->textSearch("$query")), true);
 		//var_dump($response);
 		// check if the response came back from Google OK and set the place ID, otherwise do nothing
@@ -93,21 +93,26 @@ function googlePlacesArray($restaurant, $googleId) {
 	// if a Restaurant doesn't have a Google ID, then try to find one and add it
 	if($googleId === null) {
 		// we need to be specific when searching Google's database so we don't get similarly named places back
-		$query = str_replace(" ", "+", $restaurant->getRestaurantName() . "+" . $restaurant->getRestaurantAddress1() . "+" . $restaurant->getRestaurantCity());
+		$query = urlencode(($restaurant->getRestaurantName() . "+" . $restaurant->getRestaurantAddress1() . "+" . $restaurant->getRestaurantCity()));
 		$response = json_decode(($googlePlaces->textSearch("$query")), true);
 		//var_dump($response);
 		// check if the response came back from Google OK and set the place ID, otherwise do nothing
 		// we'll assume that the first returned result is the correct one
 		if($response['status'] === 'OK') {
+			// get the Google place_id and set it in our database
 			$restaurant->setRestaurantGoogleId($response['results'][0]['place_id']);
 			$restaurant->update($pdo);
-			$attribution = $response['results'][0]['photos'][0]['html_attributions'][0];
-			$googleImage->attribution = $attribution;
-			$photoId = $response['results'][0]['photos'][0]['photo_reference'];
-			$googleImage->photoId = $photoId;
+			// make sure Google actually has a photo for the place, else it will error out
+			if(isset($response['results'][0]['photos'])) {
+				$attribution = $response['results'][0]['photos'][0]['html_attributions'][0];
+				$googleImage->attribution = $attribution;
+				$photoId = $response['results'][0]['photos'][0]['photo_reference'];
+				$googleImage->photoId = $photoId;
+			}
 		}
 	} else {
 		$response = json_decode(($googlePlaces->placeDetails("$googleId")), true);
+		// make sure Google actually has a photo for the place, else it will error out
 		if($response['status'] === 'OK' && isset($response['result']['photos'])) {
 			$attribution = $response['result']['photos'][0]['html_attributions'][0];
 			$googleImage->attribution = $attribution;
@@ -115,8 +120,9 @@ function googlePlacesArray($restaurant, $googleId) {
 			$googleImage->photoId = $photoId;
 		}
 	}
+	// if a photoID hasn't been set at this point, Google probably doesn't have a photo for the place, so give a placeholder instead
 	if(isset($googleImage->photoId) === false) {
-		// [TODO: Alex -- should return a placeholder image if it couldn't get one from Google]
+		// [TODO: Alex -- should return a placeholder image if it couldn't get one from Google; replace the echo]
 		echo("No Image available");
 	} else {
 		//	var_dump($array);
