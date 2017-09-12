4aa0eb0de3cf9987c3d28aa0562680bf3d536860
Alexander DuBois <adubois@alumni.uci.edu>
2017-09-12 11:33:16 -0600

    Moved all Google calls to single function, exception handling added https://app.asana.com/0/404235026164260/413053450778113

--- php/lib/google-places.php
+++ php/lib/google-places.php
@@ -26,46 +26,53 @@ function googlePlacesSingle($restaurant, $googleId) : stdClass {
 	// set up the Google Places call
 	$googlePlaces = new PlacesApi("$googleKey");
 	$googleImage = new stdClass();
-	// if a Restaurant doesn't have a Google ID, then try to find one and add it
-	if($googleId === null) {
-		// we need to be specific when searching Google's database so we don't get similarly named places back
-		$query = urlencode(($restaurant->getRestaurantName() . "+" . $restaurant->getRestaurantAddress1() . "+" . $restaurant->getRestaurantCity()));
-		$response = json_decode(($googlePlaces->textSearch("$query")), true);
-		//var_dump($response);
-		// check if the response came back from Google OK and set the place ID, otherwise do nothing
-		// we'll assume that the first returned result is the correct one
-		if($response['status'] === 'OK') {
-			// get the Google place_id and set it in our database
-			$restaurant->setRestaurantGoogleId($response['results'][0]['place_id']);
-			$restaurant->update($pdo);
+
+	try {
+		// if a Restaurant doesn't have a Google ID, then try to find one and add it
+		if($googleId === null) {
+			// we need to be specific when searching Google's database so we don't get similarly named places back
+			$query = urlencode(($restaurant->getRestaurantName() . " " . $restaurant->getRestaurantAddress1() . " " . $restaurant->getRestaurantCity()));
+			$response = json_decode(($googlePlaces->textSearch("$query")), true);
+			//var_dump($response);
+			// check if the response came back from Google OK and set the place ID, otherwise do nothing
+			// we'll assume that the first returned result is the correct one
+			if($response['status'] === 'OK') {
+				// get the Google place_id and set it in our database
+				$restaurant->setRestaurantGoogleId($response['results'][0]['place_id']);
+				$restaurant->update($pdo);
+				// make sure Google actually has a photo for the place, else it will error out
+				if(isset($response['results'][0]['photos'])) {
+					$attribution = $response['results'][0]['photos'][0]['html_attributions'][0];
+					$googleImage->attribution = $attribution;
+					$photoId = $response['results'][0]['photos'][0]['photo_reference'];
+					$googleImage->photoId = $photoId;
+				}
+			}
+		} else {
+			$response = json_decode(($googlePlaces->placeDetails("$googleId")), true);
 			// make sure Google actually has a photo for the place, else it will error out
-			if(isset($response['results'][0]['photos'])) {
-				$attribution = $response['results'][0]['photos'][0]['html_attributions'][0];
+			if($response['status'] === 'OK' && isset($response['result']['photos'])) {
+				$attribution = $response['result']['photos'][0]['html_attributions'][0];
 				$googleImage->attribution = $attribution;
-				$photoId = $response['results'][0]['photos'][0]['photo_reference'];
+				$photoId = $response['result']['photos'][0]['photo_reference'];
 				$googleImage->photoId = $photoId;
 			}
 		}
-	} else {
-		$response = json_decode(($googlePlaces->placeDetails("$googleId")), true);
-		// make sure Google actually has a photo for the place, else it will error out
-		if($response['status'] === 'OK' && isset($response['result']['photos'])) {
-			$attribution = $response['result']['photos'][0]['html_attributions'][0];
+		// if a photoID hasn't been set at this point, Google probably doesn't have a photo for the place, so give a placeholder instead
+		if(isset($googleImage->photoId) === false) {
+			// [TODO: Alex -- should return a placeholder image if it couldn't get one from Google; replace the echo]
+			echo("No Image available");
+		} else {
+			//	var_dump($array);
+			// Search Google for the image ID we found above, encode it into raw data to pass off to the front-end (Angular)
+			$image = base64_encode(file_get_contents("https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photoreference=$googleImage->photoId&key=$googleKey"));
+			$googleImage->image = $image;
 			$googleImage->attribution = $attribution;
-			$photoId = $response['result']['photos'][0]['photo_reference'];
-			$googleImage->photoId = $photoId;
 		}
-	}
-	// if a photoID hasn't been set at this point, Google probably doesn't have a photo for the place, so give a placeholder instead
-	if(isset($googleImage->photoId) === false) {
-		// [TODO: Alex -- should return a placeholder image if it couldn't get one from Google; replace the echo]
-		echo("No Image available");
-	} else {
-		//	var_dump($array);
-		// Search Google for the image ID we found above, encode it into raw data to pass off to the front-end (Angular)
-		$image = base64_encode(file_get_contents("https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photoreference=$googleImage->photoId&key=$googleKey"));
-		$googleImage->image = $image;
-		$googleImage->attribution = $attribution;
+	} catch(Exception $exception) {
+			$image = base64_encode(file_get_contents(dirname(__DIR__, 2) . "/public_html/images/placeholder.jpg"));
+			$googleImage->image = $image;
+			$googleImage->attribution = null;
 	}
 //	var_dump($googleImage);
 	return $googleImage;
@@ -100,7 +107,7 @@ function googlePlacesArray(array $restaurants) : array {
 			// we need to be specific when searching Google's database so we don't get similarly named places back
 			$query = urlencode(($restaurant->getRestaurantName() . "+" . $restaurant->getRestaurantAddress1() . "+" . $restaurant->getRestaurantCity()));
 			$response = json_decode(($googlePlaces->textSearch("$query")), true);
-			//var_dump($response);
+//			var_dump($response);
 			// check if the response came back from Google OK and set the place ID, otherwise do nothing
 			// we'll assume that the first returned result is the correct one
 			if($response['status'] === 'OK') {
@@ -137,5 +144,6 @@ function googlePlacesArray(array $restaurants) : array {
 		}
 		$googleImages[] = $googleImage;
 	}
+	var_dump($googleImages);
 	return $googleImages;
 }
